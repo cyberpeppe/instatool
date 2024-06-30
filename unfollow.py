dd74e885e0337a05d2390a2c56f18be9bd3fc4f0
cyberpeppe <giupeppopeppe@gmail.com>
2024-06-30 22:15:09 +0200

    Update unfollow.py

--- unfollow.py
+++ unfollow.py
@@ -3,10 +3,10 @@ import getpass
 from instagrapi import Client
 
 def print_colored(text):
-    color = "\033[92m" 
+    color = "\033[92m"  # Codice di escape ANSI per il colore verde
     print(f"{color}{text}\033[0m")
 
-config_path = r'\secret.txt'
+config_path = r'C:\Users\ReadyToUse\Desktop\Data\config\secret.txt'
 
 username = input('Inserisci l\'username: ')
 password = getpass.getpass("Inserisci la password: ")
@@ -25,32 +25,14 @@ def read_credentials(file_path):
         password = lines[1].strip()
         print_colored('Credenziali lette')
         return username, password
-
-write_credentials(config_path, username, password)
-username, password = read_credentials(config_path)
-
-try:
-    client = Client()
-    print_colored('Client configurato')
-except Exception as e:
-    if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
-        print('captcha richiesto, riprova tra alcuni minuti')
-        exit()
-try:
-    client.login(username, password)
-    print_colored('Login effettuato')
-except Exception as e:
-    if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
-        print('captcha richiesto, riprova tra alcuni minuti')
-        exit()
-try:
-    followings = client.user_following(client.user_id)
-    print_colored('Following completato')
-except Exception as e:
-    print_colored(f"Errore durante la ricerca dei seguiti: {e}")
-    client.logout()
-    print_colored('Login...')
-    client = Client()
+def remove_followers():
+    try:
+        client = Client()
+        print_colored('Client configurato')
+    except Exception as e:
+        if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+            print('captcha richiesto, riprova tra alcuni minuti')
+            exit()
     try:
         client.login(username, password)
         print_colored('Login effettuato')
@@ -58,41 +40,12 @@ except Exception as e:
         if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
             print('captcha richiesto, riprova tra alcuni minuti')
             exit()
-    time.sleep(2)
-    followings = client.user_following(client.user_id)
-    print_colored('Following completato')
-
-try:
-    followers = client.user_followers(client.user_id)
-    print_colored('Followers completato')
-except Exception as e:
-    print_colored(f"Errore durante la ricerca dei followers: {e}")
-    client.logout()
-    print_colored('Login...')
-    client = Client()
     try:
-        client.login(username, password)
-        print_colored('Login effettuato')
+        followings = client.user_following(client.user_id)
+        print_colored('Following completato')
+       
     except Exception as e:
-        if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
-            print('captcha richiesto, riprova tra alcuni minuti')
-            exit()
-    time.sleep(2)
-    followers = client.user_followers(client.user_id)
-    print_colored('Followers completato')
-
-totale = set(followings.keys()) - set(followers.keys())
-print_colored(f"Totale: {len(totale)}")
-
-for user_id in totale:
-    try:
-        client.user_unfollow(user_id)
-        print_colored(f"Unfollowed user id: {user_id}")
-    except Exception as e:
-        if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
-            print('captcha richiesto, riprova tra alcuni minuti')
-            exit()
-        print_colored(f"Errore durante l'unfollow dell'utente {user_id}: {e}")
+        print_colored(f"Errore durante la ricerca dei seguiti: {e}")
         client.logout()
         print_colored('Login...')
         client = Client()
@@ -103,8 +56,156 @@ for user_id in totale:
             if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
                 print('captcha richiesto, riprova tra alcuni minuti')
                 exit()
-        time.sleep(5)
+        time.sleep(2)
+        followings = client.user_following(client.user_id)
+        print_colored('Following completato')
 
-print_colored('Fatto')
+    try:
+        followers = client.user_followers(client.user_id)
+        print_colored('Followers completato')
+    except Exception as e:
+        print_colored(f"Errore durante la ricerca dei followers: {e}")
+        client.logout()
+        print_colored('Login...')
+        client = Client()
+        try:
+            client.login(username, password)
+            print_colored('Login effettuato')
+        except Exception as e:
+            if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+                print('captcha richiesto, riprova tra alcuni minuti')
+                exit()
+        time.sleep(2)
+        followers = client.user_followers(client.user_id)
+        print_colored('Followers completato')
+    if totale != 0:
+        totale = set(followings.keys()) - set(followers.keys())
+    else:
+        print('nessuno trovato')
+        exit()
+    print_colored(f"Totale: {len(totale)}")
+    print(totale)
 
-client.logout()
+    for user_id in totale:
+        try:
+            client.user_unfollow(user_id)
+            print_colored(f"Unfollowed user id: {user_id}")
+        except Exception as e:
+            if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+                print('captcha richiesto, riprova tra alcuni minuti')
+                exit()
+            print_colored(f"Errore durante l'unfollow dell'utente {user_id}: {e}")
+            client.logout()
+            print_colored('Login...')
+            client = Client()
+            try:
+                client.login(username, password)
+                print_colored('Login effettuato')
+            except Exception as e:
+                if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+                    print('captcha richiesto, riprova tra alcuni minuti')
+                    exit()
+            time.sleep(5)
+        
+    for user_id in followers.keys():
+        try:
+            client.user_follow(user_id)
+            print_colored(f"followed user id: {user_id}")
+        except Exception as e:
+            if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+                print('captcha richiesto, riprova tra alcuni minuti')
+                exit()
+            print_colored(f"Errore durante il follow dell'utente {user_id}: {e}")
+            client.logout()
+            print_colored('Login...')
+            client = Client()
+            try:
+                client.login(username, password)
+                print_colored('Login effettuato')
+            except Exception as e:
+                if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+                    print('captcha richiesto, riprova tra alcuni minuti')
+                    exit()
+            time.sleep(5)
+    client.logout()
+def following():
+    try:
+        client = Client()
+        print_colored('Client configurato')
+    except Exception as e:
+        if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+            print('captcha richiesto, riprova tra alcuni minuti')
+            exit()
+    try:
+        client.login(username, password)
+        print_colored('Login effettuato')
+    except Exception as e:
+        if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+            print('captcha richiesto, riprova tra alcuni minuti')
+            exit()
+    
+
+    try:
+        followers = client.user_followers(client.user_id)
+        print_colored('Followers completato')
+    except Exception as e:
+        print_colored(f"Errore durante la ricerca dei followers: {e}")
+        client.logout()
+        print_colored('Login...')
+        client = Client()
+        try:
+            client.login(username, password)
+            print_colored('Login effettuato')
+        except Exception as e:
+            if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+                print('captcha richiesto, riprova tra alcuni minuti')
+                exit()
+        time.sleep(2)
+        followers = client.user_followers(client.user_id)
+        print_colored('Followers completato')
+
+    
+    print(len(followers.keys()))
+
+    
+        
+    for user_id in followers.keys():
+        try:
+            client.user_follow(user_id)
+            print_colored(f"followed user id: {user_id}")
+        except Exception as e:
+            if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+                print('captcha richiesto, riprova tra alcuni minuti')
+                exit()
+            print_colored(f"Errore durante il follow dell'utente {user_id}: {e}")
+            client.logout()
+            print_colored('Login...')
+            client = Client()
+            try:
+                client.login(username, password)
+                print_colored('Login effettuato')
+            except Exception as e:
+                if isinstance(e, Exception) and 'ChallengeResolve' in str(e):
+                    print('captcha richiesto, riprova tra alcuni minuti')
+                    exit()
+            time.sleep(5)
+    client.logout()
+write_credentials(config_path, username, password)
+username, password = read_credentials(config_path)
+print_colored("Seleziona un'operazione:")
+print_colored("1. Unfollow utenti che non ti seguono")
+print_colored("2. Segui di nuovo tutti i followers")
+print_colored("3. Esci")
+choice = input("Inserisci il numero dell'operazione: ")
+if choice == '1':
+    remove_followers()
+elif choice == '2':
+   following()
+elif choice == '3':
+    exit()
+else:
+   print_colored("Scelta non valida. Riprova.")
+
+print_colored('Completato')
+
+
